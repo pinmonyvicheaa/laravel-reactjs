d9e8e2129916ca66801543874d1b201a0231cb6c
PhathPhanou19 <phanouphath@gmail.com>
2025-04-22 01:18:47 +0700

    footer admin

--- reactjs-ecommerce/src/Admin/components/Footer.jsx
+++ reactjs-ecommerce/src/Admin/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer(){
         <footer className="sticky-footer bg-white">
                 <div className="container my-auto">
                     <div className="copyright text-center my-auto">
-                        <span>Copyright &copy; Your Website 2021</span>
+                        <span>Copyright &copy; Your Website 2024</span>
                     </div>
                 </div>
             </footer>
